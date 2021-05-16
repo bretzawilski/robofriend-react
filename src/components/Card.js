e4382e9f3a25ed06c4f95e4772d957e34fc868b9
Bret Zawilski <bret.zawilski@gmail.com>
2021-05-16 10:43:15 +0100

    made it work

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Card.css';
 
 const Card = ({ name, email, id }) => {
   return (
-    <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+    <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5 card'>
       <img src={`https://robohash.org/${id}?100x100`} alt='portrait of robot' />
       <div>
         <h2>{name}</h2>
